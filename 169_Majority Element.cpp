4af933128c9c31c0b1bd452d66e1623327c31f2b
helloliuc <helloliuc@gmail.com>
2015-05-22 00:18:20 +0800

    Solved

---
+++ 169_Majority Element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ans, cnt=0;
+        for(int i=0; i<nums.size(); i++) {
+            if(cnt == 0) {
+                ans = nums[i];
+                cnt = 1;
+            }
+            else if(nums[i] == ans) {
+                cnt++;
+            }
+            else {
+                cnt--;
+            }
+        }
+        return ans;
+    }
+};
