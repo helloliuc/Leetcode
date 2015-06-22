108a9ef168778bbeeca15b3bf50c2e29eee61ca9
Liu Chuang <helloliuc@gmail.com>
2015-06-23 00:34:10 +0800

    Create 24_ Swap Nodes in Pairs(recrusion).cpp

---
+++ 24_ Swap Nodes in Pairs(recrusion).cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == NULL || head -> next == NULL) return head;
+        
+        ListNode* tmp = head -> next;
+        head -> next = swapPairs(tmp -> next);
+        tmp -> next = head;
+        return tmp;
+    }
+};
